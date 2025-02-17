1bfea686cc4b1b842c85a63dec35769b88f233eb
Lyppo <74815001+Lyppo@users.noreply.github.com>
2025-02-17 19:14:56 +0100

    debut de la connection

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-// This file is intentionally left blank.
+function ouvrirPopupCentre(TON_REQUEST_TOKEN, width, height) {
+    let left = (screen.width - width) / 2;
+    let top = (screen.height - height) / 2;
+    window.open("https://www.themoviedb.org/auth/access?request_token=" + TON_REQUEST_TOKEN, "popupLogin", `width=${width},height=${height},top=${top},left=${left}`);
+}
+
+document.getElementById("btnLogin").addEventListener("click", function() {
+    ouvrirPopupCentre("", 500, 600);
+});
