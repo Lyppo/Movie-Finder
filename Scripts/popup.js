118b72285cacabf2122adc2d7262fc0c035c7b73
Lyppo <74815001+Lyppo@users.noreply.github.com>
2025-03-10 02:36:15 +0100

    fix du bug de la popup

--- Scripts/popup.js
+++ Scripts/popup.js
@@ -19,6 +19,10 @@ async function openPopup(requestToken) {
     const popup = createPopup(requestToken);
 
     return new Promise((resolve) => {
+        if (!popup) {
+            log("Échec de l'ouverture du popup", "error", {}, "Popup");
+            return resolve(false);
+        }
 
         const messageListener = (event) => {
             log('Message reçu du popup', 'log', { eventData: event.data }, 'Popup');
@@ -27,13 +31,29 @@ async function openPopup(requestToken) {
                 window.removeEventListener("message", messageListener);
                 return resolve(true);
             }
+            else {
+                log("message inconnu reçu du popup", "error", { eventData: event.data }, "Popup");
+                popup.close();
+                return resolve(false);
+            }
         };
 
         window.addEventListener("message", messageListener);
 
-        setTimeout(() => {
-            popup.close();
-            log('Popup fermé après délai de 30 secondes', 'error', null, 'Popup');
-        }, 30000);
+         function checkPopupClosed() {
+            if (!popup || popup.closed) {
+                log("Le popup semble fermé, attente de confirmation...", "log", null, "Popup");
+
+                setTimeout(() => {
+                    log("Le popup est bien fermé, résolution de la promesse", "log",null, "Popup");
+                    window.removeEventListener("message", messageListener);
+                    return resolve(false);
+                }, 1500);
+            } else {
+                return requestAnimationFrame(checkPopupClosed);
+            }
+        }
+
+        return requestAnimationFrame(checkPopupClosed);
     });
 }
