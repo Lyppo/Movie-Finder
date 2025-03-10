ede156408f73725d5bcf273476cb824980c68bc5
Lyppo <74815001+Lyppo@users.noreply.github.com>
2025-03-10 03:11:19 +0100

    ameilloration de auth (surtout les log)

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -1,66 +1,54 @@
 async function createRequestToken() {
-    let redirect_to = window.location.origin + window.location.pathname + "/popup.html";
 
-    log('Création d\'un token de requête', 'request', { redirect_to }, 'Authentification');
+    const redirect_to = window.location.origin + window.location.pathname + "/popup.html";
 
     const data = await requestAuth('POST', 'https://api.themoviedb.org/4/auth/request_token', { redirect_to });
 
-    if (data && data.request_token) {
-        log('Token de requête créé avec succès', 'success', { request_token: data.request_token }, 'Authentification');
+    if (data.success) {
+        log('Token de requête créé avec succès', 'success', { request_token: data.request_token }, 'Request Token');
         return data.request_token;
-    } else {
-        return log('Erreur lors de la création du token de requête', 'error', data, 'Authentification');
-    }
+    } else return log('Erreur lors de la création du token de requête', 'error', data, 'Request Token');
 }
 
 async function createAccessToken(tmpToken) {
-    log('Création d\'un token d\'accès avec le token temporaire', 'request', { tmpToken }, 'Authentification');
 
     const data = await requestAuth('POST', 'https://api.themoviedb.org/4/auth/access_token', { request_token: tmpToken });
 
-    if (data && data.account_id && data.access_token) {
+    if (data.success) {
         ACCOUNT_ID = data.account_id;
         ACCESS_TOKEN = data.access_token;
         setCookie("ACCOUNT_ID", ACCOUNT_ID);
         setCookie("ACCESS_TOKEN", ACCESS_TOKEN);
 
-        log('Token d\'accès créé avec succès', 'success', { ACCOUNT_ID, ACCESS_TOKEN }, 'Authentification');
-    } else {
-        log('Erreur lors de la création du token d\'accès', 'error', data, 'Authentification');
-    }
+        log('Token d\'accès créé avec succès', 'success', { account_id: ACCOUNT_ID, access_token: ACCESS_TOKEN }, 'Access Token');
+    } else log('Erreur lors de la création du token d\'accès', 'error', data, 'Access Token');
 }
 
 async function createSession() {
-    log('Création de la session', 'request', { ACCESS_TOKEN }, 'Authentification');
 
     const data = await requestAuth('POST', 'https://api.themoviedb.org/3/authentication/session/convert/4', { access_token: ACCESS_TOKEN });
 
-    if (data && data.session_id) {
+    if (data.success) {
         SESSION_ID = data.session_id;
         setCookie("SESSION_ID", SESSION_ID);
 
-        log('Session créée avec succès', 'success', { SESSION_ID }, 'Authentification');
-    } else {
-        log('Erreur lors de la création de la session', 'error', data, 'Authentification');
-    }
+        log('Session créée avec succès', 'success', { SESSION_ID }, 'Session ID');
+    } else log('Erreur lors de la création de la session', 'error', data, 'Session ID');
 }
 
 async function logoutRequest() {
-    log('Déconnexion de l\'utilisateur', 'request', { ACCESS_TOKEN }, 'Authentification');
 
     const data = await requestAuth('DELETE', 'https://api.themoviedb.org/4/auth/access_token', { access_token: ACCESS_TOKEN });
 
-    if (data) {
-        log('Utilisateur déconnecté avec succès', 'success', data, 'Authentification');
-    } else {
-        log('Erreur lors de la déconnexion', 'error', data, 'Authentification');
-    }
+    if (data.success) {
+        log('Utilisateur déconnecté avec succès :\n' + data.status_message , 'success', null, 'Logout Request');
+    } else log('Erreur lors de la déconnexion', 'error', data, 'Logout Request');
 
     return data;
 }
 
 async function requestAuth(type, url, content) {
-    log('Envoi de la requête d\'authentification', 'request', { type, url, content }, 'Réseau');
+    log('Envoi de requête d\'authentification', 'request', { type, url, content }, 'Authentification');
 
     try {
         const response = await fetch('https://tmdb-request.antodu72210.workers.dev/', {
@@ -69,17 +57,21 @@ async function requestAuth(type, url, content) {
             body: JSON.stringify({ url: url, content: content })
         });
 
+        log('Réponse de l\'authentification', 'response', response, 'Authentification');
+
         const data = await response.json();
-        log('Réponse reçue de l\'API', 'success', data, 'Réseau');
 
         return data;
+
     } catch (error) {
 
+        let module;
+        
         try {
-            const module = await import('./localAdressDebug.js');
-            return await module.requestAuth(type, url, content);
-        } catch (error) {
-            return log('Erreur lors de la requête d\'authentification', 'error', error, 'Réseau');
+            module = await import('./localAdressDebug.js');
+        } catch (err) {
+            return log('Erreur lors de la requête d\'authentification', 'error', error, 'Authentification');
         }
+        return await module.requestAuth(type, url, content);
     }
 }
